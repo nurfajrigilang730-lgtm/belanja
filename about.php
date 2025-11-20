a74ebc722665a29f445e33da79456388ad790d19
nurfajrigilang730-lgtm <nurfajrigilang730@gmail.com>
2025-11-20 09:22:58 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Tentang Kami</b></h2>
+	<p class="text-justify">Sekolah Pencetak Wirausaha (SPW) adalah program pendidikan yang bertujuan untuk membentuk karakter dan keterampilan kewirausahaan siswa agar mampu berwirausaha, bukan hanya mencari pekerjaan. Program ini memberikan pelatihan teori dan praktik kewirausahaan yang terpadu, mulai dari pembentukan kelompok, pendampingan, hingga pengelolaan usaha secara mandiri. Tujuannya adalah menumbuhkan semangat wirausaha, meningkatkan kreativitas, dan menciptakan lapangan kerja. </p>
+<p>
+SPW SMK Pembangunan Bagan Batu adalah singkatan dari Sekolah Pencetak Wirausahawan, yang merupakan sebuah program atau inisiatif di SMKS Pembangunan Bagan Batu. Program ini bertujuan untuk menciptakan lulusan yang kreatif, inovatif, dan kompetitif, serta memiliki kemampuan wirausaha.</p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
---
+++ footer.php
@@ -0,0 +1,31 @@
+
+	<footer style="border-top: 4px solid #66ff66;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #66ff66"><b>SPW SMK Pembangunan</b></h3>
+					<p>Jl. Dr. Sutomo Bagan Batu</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +6281364052264</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> smkpyppbb@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href="http://localhost/TokoSPW/produk.php"  style="color: #000">Produk</a></p>
+					<p><a href="http://localhost/TokoSPW/about.php"  style="color: #000">Tentang kami</a></p>
+					<p><a href="https://wa.me/+6281364052264?text=hai, saya mau pesan"  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #66ff66; padding: 5px; color: #fff; text-align: center;">
+			<span>Copyright&copy; </span>
+		</div>
+	</footer>
+
+</body>
+</html>
